7ff2b4f083c12fc1c4e5948bb12a37a9822eb79c
CMLTTN <cemaldim@gmail.com>
2023-07-25 12:02:00 +0300

    parent

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "James",
+      email: "james@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Dimly",
+      email: "dimly@gmail.com",
+    },
+  ];
   return (
-  <div  className='ui container' >
-
-
-    <Header/>
-    <AddContact/>
-    <ContactList/>
-  </div>
-  )
+    <div className="ui container">
+      <Header />
+      <AddContact />
+      <ContactList contacts={contacts} />
+    </div>
+  );
 }
 
 export default App;
